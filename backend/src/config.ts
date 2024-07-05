986f17f9014ed1ace7726a84d5d7b581a76182b2
julasweta <julasweta@ukr.net>
2024-07-05 23:46:40 +0300

    env

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,8 +1,21 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize("snake_db", "postgres", "pass", {
-  host: "localhost",
-  dialect: "postgres",
+dotenv.config(); 
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbDialect = process.env.DB_DIALECT;
+
+if (!dbName || !dbUser || !dbPassword || !dbHost || !dbDialect) {
+  throw new Error("One or more database environment variables are not defined");
+}
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: dbDialect as any, 
   logging: false,
 });
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routing/router";
+import { store } from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
